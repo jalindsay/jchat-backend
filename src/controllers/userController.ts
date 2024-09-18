da92ee23acc11a42ffcd7c20dd8127250642a54a
James Lindsay <jalindsay22@gmail.com>
2024-09-18 17:26:24 +0100

    login endpoint returns username and ID

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -85,7 +85,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
       secretKey,
       { expiresIn: "1h" }
     );
-    res.json({ token });
+    res.json({ token, username: user.username, id: user.id });
   } catch (error) {
     console.error("Error querying the database:", error);
     res.status(500).send("Internal Server Error");
